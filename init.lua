1504ac2366a23a3007e685cb73b6f5f23be555c0
Ruei <ruei@null.local>
2024-04-01 23:24:36 +0800

    udpate config with new plugins :sparkles:

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("options")
+require("remap")
 require("plugins")
 
--- lua/options.lua
+++ lua/options.lua
@@ -10,10 +10,23 @@ vim.opt.wrap = false
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.clipboard = "unnamedplus"
 
-vim.opt.scrolloff = 999
+vim.opt.scrolloff = 8 
 
 vim.opt.virtualedit = "block"
 
@@ -23,5 +36,8 @@ vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
 
-vim.g.mapleader = " "
+vim.opt.updatetime = 50
 
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
